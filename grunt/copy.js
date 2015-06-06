19b7f8fdf2f706f36cc46cbf5af8ba05d55a17b8
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2015-06-06 15:47:39 +0300

    Updated Grunt project.

---
+++ grunt/copy.js
@@ -0,0 +1,21 @@
+module.exports = {
+	build: {
+		cwd: '.',
+		expand: true,
+		src: [
+			'**/*',
+			'!**/assets/**',
+			'!**/grunt/**',
+			'!**/node_modules/**',
+			'!.DS_Store',
+			'!**/.DS_Store',
+			'!.gitignore',
+			'!Gruntfile.js',
+			'!README.txt',
+			'!*.sublime-project',
+			'!*.sublime-workspace',
+
+		],
+		dest: '../build/<%= package.name %>/'
+	}
+}
